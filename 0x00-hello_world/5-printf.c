99126a79fb43820225b9711b581ea684e302f73c
Juli868 <bugijul@gmail.com>
2023-03-16 16:31:11 -0700

    Third trial

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-/*
- * main() - is the starting of the program
- *Return: shows the end with 0 if succeded
+/**
+ * main()- is the starting of the program
+ *Return:always prints 0 for success
 */
 int main(void)
 {
