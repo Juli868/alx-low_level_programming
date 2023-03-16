a2be3ef88a8044a9c22ada7dc1a2fbed2691387b
Juli868 <bugijul@gmail.com>
2023-03-16 15:47:17 -0700

    Second trial

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * This program will display a sentence
- * using puts
+ * main()- as the start
+ * Return: if succeeded 0 will be printed due to in main
  */
-int main(void)
+int main(void)/*this is the start of ec=xcution*/
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+	return (0);/* if the program displays 0 it means its a success*/
 }
