6ddb8bf3ab4edb75263dab25e9f156b9a4916f85
Juli868 <bugijul@gmail.com>
2023-03-16 15:05:04 -0700

    Second trial

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-int main(void)
+/*
+ * main is the starting of the program
+ *return shows the end with 0 if succeded
+*/ 
+	int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
