155674c3b1c1538a8e6207a2a45ad150511280c5
Juli868 <bugijul@gmail.com>
2023-03-16 08:32:08 -0700

    Second trial

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+/**
+ * This program will display a sentence
+ * using puts
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
