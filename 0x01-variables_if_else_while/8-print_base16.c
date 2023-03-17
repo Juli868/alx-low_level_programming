e5460457d600dcd9192eed112e1c14140a586b00
Juli868 <bugijul@gmail.com>
2023-03-17 11:53:45 -0700

    nineth commiit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main- starts excution
+* Return: ends program
+*/
+int main(void)
+{
+	char hex[16] = "0123456789abcdef";
+	int i = 0;
+
+	while (i < 17)
+	{
+		putchar(hex[i]);
+		i++;
+	}
+	putchar"\n"
+		return (0);
+}
