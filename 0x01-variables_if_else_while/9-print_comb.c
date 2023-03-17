cf3db9955ae470d11cec91a42cf9256180efc01f
Juli868 <bugijul@gmail.com>
2023-03-17 15:19:22 -0700

    Tenth commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main- starts the excution
+* Return: ends the program
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
