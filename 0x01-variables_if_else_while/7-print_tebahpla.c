780c59d0aae1157bf8dd5895a3a67e198c61f619
Juli868 <bugijul@gmail.com>
2023-03-17 11:32:28 -0700

    Eight commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - starts the excution
+*Return: ends the excution
+*/
+int main(void)
+{
+	char alpha[26] = {abcdefghijklmnopqrstuvwxyz};
+	int i = 25;
+
+	while (i >= 0)
+	{
+		putchar(alpha[i]);
+		i--
+	}
+	return(0)
+}
