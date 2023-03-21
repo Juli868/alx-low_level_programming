2dc4842e1edeff794bafbd649cd6eff753b362ae
Juli868 <bugijul@gmail.com>
2023-03-21 14:15:21 -0700

    Second trial

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-# include <stdio.h>
+# include "main.h"
 /**
 * main - starts the excution of the program
 * Return: marks the end of the excution
 */
 int main(void)
 {
-	printf("_putchar\n");
+	jul('_');
+	jul('p');
+	jul('u');
+	jul('t');
+	jul('c');
+	jul('h');
+	jul('a');
+	jul('r');
+	jul('\n');
 	return (0);
 }
