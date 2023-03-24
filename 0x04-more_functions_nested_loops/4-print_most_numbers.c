6310afe9448d668978b377d5106bf414bcb7a65c
Juli868 <bugijul@gmail.com>
2023-03-24 07:43:35 -0700

    Fifth task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ print_most_numbers - prints numbers 0to 9excluding 2,4
+ *_putchar - prints on the console
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58 ; c++)
+	{
+		if (c != 50 || c != 52)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
