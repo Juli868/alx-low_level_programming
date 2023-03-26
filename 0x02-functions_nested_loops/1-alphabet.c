c6bba8341d1ca3688e2ddd35c7a10c5444fcc38f
Juli868 <bugijul@gmail.com>
2023-03-26 08:41:10 -0700

    Betty correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * orint_alphabet : will print all aphabets 
- */
+* print_alphabet - will print all aphabets
+*/
 
 void print_alphabet(void)
 {
-        char a[26] = "abcdefghijklmnopqrstuvwxyz";
-        int i;
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-        for (i = 0 ; i < 26; i++)
-        {
-                _putchar(a[i]);
-        }
-        _putchar('\n');
+	for (i = 0 ; i < 26; i++)
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
 }
