8310a63f3fb4f1076dd272ef99f184fb1cab6c9b
Juli868 <bugijul@gmail.com>
2023-03-27 14:28:40 -0700

    Last trial on fourth task

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *_puts - will print element of a string
+ *@str: the string to be orinted
  */
 void _puts(char *str)
 {
 	int i = 0;
-	
+
 	while (str[i])
 	{
 		_putchar(str[i]);
