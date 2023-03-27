8c87d62e881f1e382ef291d991053e25781e83d5
Juli868 <bugijul@gmail.com>
2023-03-27 09:08:10 -0700

    Second task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *swap_int - will swap the values of twoi numbers
+ *@a: first number to be swaped
+ *@b: Second number for swap
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*b = *a;
+	temp = *b;
+}
