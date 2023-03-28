580dd25e228cc8c8669502bcb09bcfaa2f51fad4
Juli868 <bugijul@gmail.com>
2023-03-28 09:04:49 -0700

    Seventh task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,17 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int j = 0;
 
 	while (str[i])
 	{
-		_putchar(str[i]);
-		i = i + 2;
+		i++;
+	}
+	i--;
+	while (j <= i)
+	{
+		_putchar(str[j]);
+		j = j + 2;
 	}
 	_putchar('\n');
 }
