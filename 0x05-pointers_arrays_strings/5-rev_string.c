22d6caad552c8d4d90388a264d4289146c2c5de2
Juli868 <bugijul@gmail.com>
2023-03-28 07:26:10 -0700

    second half

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*rev_string - will save a reverse form of a string
+*@s: string to be reversed
+*/
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char new_string[] = {};
+
+	while (s[1])
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		new_string[i] = s[j];
+		i--;
+		j++;
+	}
+	new_string[] = s[]
+}
+
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_strcpy - will copy the elements of a string to another
+ * @src: first string
+ *@dest: second string
+ *Return: prints the swaped value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return (dest);
+}
