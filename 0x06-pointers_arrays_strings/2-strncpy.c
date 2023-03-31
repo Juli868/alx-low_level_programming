9ca1ac7bfd83c250ef5bc04681b5e5aafd7418ae
Juli868 <bugijul@gmail.com>
2023-03-31 14:35:37 -0700

    Third task correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,23 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+	int j = 0;
 
-	while (i < n && src[i] != '\0')
+	while (dest[i])
 	{
-		dest[i] = src[i];
 		i++;
+	}
+	while (src[j])
+	{
+		j++;
+	}
+	if (j < i)
+	{
+		for (i = 0; i < n && src[i] != '\0'; i++)
+		{
+			dest[i] = src[i];
+			i++;
+		}
 	}
 	return (dest);
 }
