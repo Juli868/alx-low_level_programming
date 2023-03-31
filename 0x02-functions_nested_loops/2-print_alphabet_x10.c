41882377022c5b752cd91f7acdbd15f6d6c6a854
Juli868 <bugijul@gmail.com>
2023-03-31 08:45:02 -0700

    last trial

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,11 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (i = 0; i < 11; i++)
+	for (i = 1; i < 11; i++)
 	{
 		for (j = 97; j < 123 ; j++)
 		{
-		_putchar("%c",j);
+			_putchar("%c", j);
 		}
 		_putchar('\n');
 	}
