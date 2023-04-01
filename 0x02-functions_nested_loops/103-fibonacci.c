90bcfcebcb72423070c5823fccdcdf801dc234fc
Juli868 <bugijul@gmail.com>
2023-04-01 16:32:26 -0700

    103 task

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - starts the excution
+ * Return: 0 as default
+ */
+int main(void)
+{
+	int a, b, c, i, sum, final;
+
+	sum = 0;
+	a = 1;
+	b = 2;
+	final = 0;
+	for (i = 0; sum < 4000000; i++)
+	{
+		c = a + b;
+		sum = sum + a;
+		if (c % 2 == 0)
+		{
+			final = final + c;
+			printf("%d, ", c);
+		}
+		a = b;
+		b = c;
+	}
+	printf("\n%d", final);
+	return (0);
+}
