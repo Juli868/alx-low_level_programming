29434f53fb72766493f8060d8b7f818c7219edf3
Juli868 <bugijul@gmail.com>
2023-04-03 17:40:23 -0700

    nineth task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *set_string - will set the first srting to the second one
+ *@s: string to change
+ *@to: string to change to
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
