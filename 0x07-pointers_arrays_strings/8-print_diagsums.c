1e37b607515e244ecc6d69ec1fa40d168125ec26
Juli868 <bugijul@gmail.com>
2023-04-03 18:40:14 -0700

    eigth task new correction

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ *print_diagsums - prints the diagonal sum of a perfect square
+ *@a: oiunter to the array to consider
+ *@size: size of the array
  */
 void print_diagsums(int *a, int size)
 {
-	int i, left, right;
+	int i, j, left, right;
 
 	left = 0;
 	right = 0;
-	size --;
+	size--;
 	for (i = 0; i <= size; i++)
 	{
-		left = left + a[i*size +i];
+		for (j = 0; i <= size; j++)
+		{
+			if (i == j)
+			{
+				left = left + a[i * j];
+			}
+		}
 	}
 	for (i = 0; i <= size; i++)
-	{
-		right = right + a[i*size+i];
+	{right = right + a[i * size];
 		size--;
 	}
 	printf("%d , %d\n", left, right);
