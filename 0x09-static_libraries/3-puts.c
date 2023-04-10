d8887b3762a9c541e79ab952101e830ab3342d84
Juli868 <bugijul@gmail.com>
2023-04-10 09:45:18 -0700

    First task second trial

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _puts- prints a string to the screen
+ * @s: sttring to print
+ */
+void _puts(char *s)
+{
+    puts(s);
+}
+
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - starts the implementetion of the programm
+ * Return: 0 as default
+ */
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
