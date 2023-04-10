884d67b2de8656f4359311c3705df26ad1eb2c72
Juli868 <bugijul@gmail.com>
2023-04-10 13:44:21 -0700

    second task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - marks the start of excution
+ *@argc: argument number
+ *@argv: argument array
+ *Return: 0 as default
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf ("%d\n", argc - 1);
+	return (0);
+}
