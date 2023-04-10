20ac32148c159ccbe8473c5ee80ba0620c8a17df
Juli868 <bugijul@gmail.com>
2023-04-10 14:00:07 -0700

    Third task thie=rd trial

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,9 +6,10 @@
  *@argv: arguments in string form
  *Return: 0 as default
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i = 0;
+
 	while (i < argc)
 	{
 		printf("%s\n", argv[i]);
