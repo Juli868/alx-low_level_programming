37c8081b192dc063fda1ef05ddd4b01f1d34fe24
Juli868 <bugijul@gmail.com>
2023-04-10 13:57:33 -0700

    THird task second trial

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *
- *
- *
- *
+ *main - starts the excution process
+ *@argc: number of arguments
+ *@argv: arguments in string form
+ *Return: 0 as default
  */
 int main (int argc, char *argv[])
 {
-	int i = 1;
+	int i = 0;
 	while (i < argc)
 	{
 		printf("%s\n", argv[i]);
