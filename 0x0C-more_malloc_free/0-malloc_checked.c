be0c763af4c3ffeb58c98b810af7e2b0d5ddc758
Juli868 <bugijul@gmail.com>
2023-04-13 11:59:27 -0700

    First task second trial

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *
- *
- *
+ *malloc_checked - alloctes memory 
+ *@b: the length of the the memory allocation
+ *Return: the location of the memory
  */
  void *malloc_checked(unsigned int b)
 {
 	unsigned int *p;
+	if (b > 0)
+		p = malloc(b * sizeof(unsigned int));
 	if (b <= 0)
 		exit(98);
-	else
-		p = malloc(b * sizeof(unsigned int));
 	return (p);
 }
