b818a8494db2c46e59ef2582410dcf24c315345e
Juli868 <bugijul@gmail.com>
2023-04-13 16:18:02 -0700

    Last task second trial

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
- *
- *
- *
- *
+ *array_range - shows the addres by which the array may possibly take
+ *@min: first element
+ *@max: last element
+ *Return: the allocated memory
  */
 int *array_range(int min, int max)
 {
@@ -17,11 +16,12 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	i = 0;
-	while(min <= max)
+	while (min < max)
 	{
 		p[i] = min;
 		i++;
 		min++;
 	}
+	p[i + 1] = max;
 	return (p);
 }
