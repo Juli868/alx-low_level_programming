4062e02c2f1bf1f6830d6a81213a832dff78599d
Juli868 <bugijul@gmail.com>
2023-04-14 10:08:12 -0700

    Second task retrial fifth commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,24 +17,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	l2 = 0;
 	count = 0;
 	while (s1[l1])
-	{
 		l1++;
-	}
 	while (s2[l2])
-	{
 		l2++;
-	}
-	l1--;
 	if ( l2 <= 1 || l1 <= 1)
 	{
 		return (NULL);
 		exit(0);
 	}
-	else
-	{
-		if (l2 < n)
+	if (l2 < n)
 		{
-			out = malloc(l1 + l2);
+			n= l2;
+		}
+			out = malloc(l1 + n);
 			l2 = 0;
 			while (out[count])
 			{
@@ -47,27 +42,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 				}
 				count++;
 			}
-			out[count] = '\0';
-		}
-			else if (l2 > n)
-			{
-				out = malloc(l1+n);
-				l2 = 0;
-				while (out[count])
-				{
-					if (count < l1)
-					{
-						out[count] = s1[count];
-					}
-					else
-					{
-						out[count] = s2[l2];
-						l2++;
-					}
-					count++;
-				}
-				out[count] = '\0';
-			}
-	}
-	return (out);
+			return (out);
 }
