03e7295e391940f98a6063ae8e7ee4b8b3d05842
Juli868 <bugijul@gmail.com>
2023-04-15 17:17:45 -0700

    Fourt tawsk retrial

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,26 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum, j, i, d;
+	int sum, i;
 
-	i = 1;
-	j = 0;
+	i = 0;
 	sum = 0;
 	while (i < argc)
 	{
-		d = atoi(argv[i]);
-		if (_isdigit(d) != 0)
-		{
-			sum = sum + atoi(argv[i]);
-		}
-		else
-			j++;
+		sum = sum + argv[i];
 		i++;
-	}
-	if (j != 0)
-	{
-		printf("Error");
-		return (1);
 	}
 	else
 	{
