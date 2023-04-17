1c4f56b7d3cca9999d21f35d9f7d9eaba06f3e9b
Juli868 <bugijul@gmail.com>
2023-04-17 15:26:26 -0700

    Second and third task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - provides the vaue to all elements of the structure dog
+ * @name: dog name
+ * @age: dog's age
+ * @owner: dog's owner
+ * @d: pointer to thee structure
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	(*d).name = name;
+       	(*d).age = age;
+	(*d).owner = owner;
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ *print_dog - prints the information about the dog
+ *@d: the location of the struct
+ */
+void print_dog(struct dog *d)
+{
+	if ( (d)->name == NULL)
+	{
+		printf("(nil)");
+	}
+	if ((d)->owner == NULL)
+	{
+		printf("(nil)");
+	}
+	if (d == NULL)
+	{
+		;
+	}
+	else
+	{
+		printf("Name: %s\nAge: %f\nOwner: %s\n",(d)->name, (d)->age, (d)->owner);
+	}
+}
