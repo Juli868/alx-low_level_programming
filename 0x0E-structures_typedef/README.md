82d87317e7623641f35d1ac6a4b79a5d8b9f030c
Juli868 <bugijul@gmail.com>
2023-04-17 14:37:54 -0700

    First task

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+This project emphasise on getting the knowlegde on how to better use struct in C programming
+hence making sure to better understand the whole project so that to get thwe whole story.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - basic information about a dog
+ * @name: name of the dog
+ * @age:age of the dog
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
