38bf0c82b28c6f9e62c573df11a2237b7111d985
Juli868 <bugijul@gmail.com>
2023-04-19 14:34:52 -0700

    First two tasks

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+*array_iterator - prints elements of an array
+*@array: the address of the array
+*@size: number of elements contained by the aray
+*@action:pointer
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
