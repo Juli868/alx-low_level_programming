b58e1c70f41d42fed0f0f125051c97dc3ea4cf8c
Juli868 <bugijul@gmail.com>
2023-04-28 12:27:08 -0700

    sixsth task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *_sqrt_recursion - calculates the squareroot of provided number
+ *@n: the number to look for its squareroot
+ *Return: the squareroot
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (calculator(n, 0));
+}
+/**
+ * calculator - finds the calculator
+ * @n: the number to look for
+ * @counter: the checker to look for
+ * Return: the actual number of of squareroot
+ */
+int calculator(int n, int counter)
+{
+	if (n < 0)
+		return (-1);
+	else
+	{
+		if ((counter * counter) == n)
+			return(counter);
+		else
+			return (calculator(n, counter + 1));
+	}
+}
