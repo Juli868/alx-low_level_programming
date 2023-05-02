e8be371af2fc66cae81bf2ac5cc13053feffd8c9
Juli868 <bugijul@gmail.com>
2023-05-02 12:48:21 -0700

    Sixth task

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,12 +1,18 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * free_listi2 - will set the head to null and the address of the next node to null
+ * free_listi2 - will set null to all addresses in the list
  * @head:address of first node
  */
- void free_listint2(listint_t **head)
+void free_listint2(listint_t **head)
 {
+	listint_t *temp_add;
+
 	while (*head)
-		free(head->next);
+	{
+		temp_add = (*head)->next;
+		head = NULL;
+		*head = temp_add;
+	}
 	*head = NULL;
 }
