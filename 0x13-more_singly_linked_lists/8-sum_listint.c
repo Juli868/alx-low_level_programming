1d72cc259e6cd9320d40637931949b0509a4abf1
Juli868 <bugijul@gmail.com>
2023-05-03 13:05:37 -0700

    Nineth task redo

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_listint:calculates the sum of all nodes in list
+ * sum_listint- calculates the sum of all nodes in list
  * @head: the address of the first node
  * Return:the sum of all nodes
  */
@@ -9,7 +9,7 @@ int sum_listint(listint_t *head)
 	int sum;
 
 	sum = 0;
-	while(head)
+	while (head)
 	{
 		sum = sum + head->n;
 		head = head->next;
