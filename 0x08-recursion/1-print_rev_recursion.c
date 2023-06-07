0957feebdc288dca0fe5c2e59562e770e84e66e4
juli868 <bugijul@gmail.com>
2023-06-07 13:04:12 -0700

    SEcond task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_string -helps in printing the given string
+ * @a: string to print from behind
+ * @len:lemght of the string
+ */
+void print_string(char *a, int len)
+{
+	if (len > 0)
+	{
+		_putchar(a[len]);
+		print_string(a, len - 1);
+	}
+	else
+		_putchar('\n');
+}
+/**
+ *_print_rev_recursion - returns the string from behind
+ *@s: string to print
+ */
+void _print_rev_recursion(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	print_string(s, len);
+}
