0a91f88d9a482d069336d6922f203c23e1cfe717
juli868 <bugijul@gmail.com>
2023-06-08 04:19:37 -0700

    Thirs task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,29 @@
 #include "main.h"
 /**
- *
- *
- *
+ *real_finder - is the function that actually finds the length required
+ *@s: the string to find lenght for
+ *@counter: keeps the length found so far
+ *Return: the founf length calculated
+ */
+int real_finder(char *s, int counter)
+{
+	if (s[counter])
+	{
+		counter++;
+		return (real_finder(s, counter));
+	}
+	return (counter);
+}
+/**
+ *_strlen_recursion - will calculate the length of a given string
+ *@s: string to find its length
+ *Return: the length in number
  */
 int _strlen_recursion(char *s)
 {
 	int counter;
 
 	counter = 0;
-	if (*s)
-	{
-		counter ++;
-		_strlen_recursion
+
+	return (real_finder(s, counter));
+}
