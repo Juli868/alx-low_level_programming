56d8d892d953469fe63ac9f59b085836d2596645
juli868 <bugijul@gmail.com>
2023-06-08 13:50:51 -0700

    Last task last trial

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
  *prime_counter - is the finder for prime number
  *@n: number to checker
  *@i: counter for divors
- *counter:number to check if divior
+ *@counter: number to check if divior
  *Return: 1 for true or 0 if not
  */
 int prime_counter(int n, int i, int counter)
