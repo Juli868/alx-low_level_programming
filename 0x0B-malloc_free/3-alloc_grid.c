a6540beeb03c3f1faae70d07a7c08b21cad0c254
juli868 <bugijul@gmail.com>
2023-06-14 14:04:27 -0700

    Fourth task redo

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,19 +7,26 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int counter_h, counter_w;
+	int counter_h, counter_w, counter;
 	int **grid;
 
 	counter_h = 0;
 	counter_w = 0;
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = malloc(sizeof(int) * (height * width));
+	grid = malloc(sizeof(int*) * height);
+	for (counter = 0; counter < height; counter++)
+		grid[counter] = malloc(sizeof(int) * width);
 	while (counter_h < height)
 	{
 		while (counter_w < width)
+		{
 			grid[counter_h][counter_w] = 0;
+			counter_w++;
+		}
 		counter_w = 0;
+		counter_h++;
 	}
 	return (grid);
+	free(grid);
 }
