a1bd8814982161455f47197c3f36ebcea728a612
juli868 <bugijul@gmail.com>
2023-06-18 06:11:54 -0700

    Forth task redo

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	difference = (max - min);
+	difference = (max - min) + 1;
 	array = malloc(sizeof(int) * difference);
 	if (array == NULL)
 		return (NULL);
