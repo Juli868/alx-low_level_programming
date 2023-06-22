a99e699b652cc89d489e23ce728f6de0ead18e07
juli868 <bugijul@gmail.com>
2023-06-22 15:38:04 -0700

    Third task

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,21 +8,21 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int counter;
-	char * new;
+	char *new;
 	va_list mine;
 
 	va_start(mine, n);
 	for (counter = 0; counter < n; counter++)
 	{
-		new = (va_arg(mine , char*));
+		new = (va_arg(mine, char *));
 		if (new == NULL)
-			printf ("(nil)");
-		if (counter != (n - 1) && separator != NULL)
+			printf("(nil)");
+		else if (counter != (n - 1) && separator != NULL)
 		{
 			printf("%s%s", new, separator);
 		}
 		else
-			printf("%s", va_arg(mine, char*));
+			printf("%s", new);
 	}
 	printf("\n");
 	va_end(mine);
