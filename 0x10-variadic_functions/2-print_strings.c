45ca7bc17d53feff7a398fb39c922b5e28479383
juli868 <bugijul@gmail.com>
2023-06-22 14:29:05 -0700

    Third task

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_strings - will print strings given
+ * @n: number of strings to print
+ *@separator: symbol to put between strings.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int counter;
+	char * new;
+	va_list mine;
+
+	va_start(mine, n);
+	for (counter = 0; counter < n; counter++)
+	{
+		new = (va_arg(mine , char*));
+		if (new == NULL)
+			printf ("(nil)");
+		if (counter != (n - 1) && separator != NULL)
+		{
+			printf("%s%s", new, separator);
+		}
+		else
+			printf("%s", va_arg(mine, char*));
+	}
+	printf("\n");
+	va_end(mine);
+}
