dd3bfd399f018ea6b10749dfb952292a698367f6
juli868 <bugijul@gmail.com>
2023-06-30 16:08:49 -0700

    Last task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 /**
- * free_list: will clear the list
+ * free_list- will clear the list
  * @head: first element
  */
 void free_list(list_t *head)
 {
-	list_t *temp;	
+	list_t *temp;
+
 	while (head)
 	{
 		free(head);
