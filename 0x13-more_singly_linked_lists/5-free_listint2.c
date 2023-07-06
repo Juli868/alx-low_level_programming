4bab76b3ef671ac4abafcb0d28d3ce9ea25f0917
juli868 <bugijul@gmail.com>
2023-07-06 11:06:17 -0700

    Sixth task re-do

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,6 @@
 #include "lists.h"
-/**free_listint2 - will free the memory that was occupied by the list
+/**
+ * free_listint2 - will free the memory that was occupied by the list
  * @head: pointer to the first node
  */
 void free_listint2(listint_t **head)
