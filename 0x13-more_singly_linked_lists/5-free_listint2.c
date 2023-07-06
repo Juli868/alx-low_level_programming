0ae28ad419361e3418d825b6e998e07ece146c6c
juli868 <bugijul@gmail.com>
2023-07-06 10:59:06 -0700

    Next two tasks

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**free_listint2 - will free the memory that was occupied by the list
+ * @head: pointer to the first node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_add;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		temp_add = (*head)->next;
+		free(*head);
+		*head = temp_add;
+	}
+	free(*head);
+}
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint - will delete first node
+ * @head: pointer to the first node
+ * Return: the n element of the new first node
+ */
+int pop_listint(listint_t **head)
+{
+	int prev;
+	listint_t *tmp;
+
+	tmp = *head ;
+	if (tmp == NULL)
+		return (0);
+	prev = tmp->n;
+	tmp = tmp->next;
+	free(*head);
+	*head = tmp;
+	return (prev);
+	free(tmp);
+}
