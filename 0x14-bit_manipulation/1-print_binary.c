a4b46bd0fbaa7ea566183496bb442e2bbf445e7c
juli868 <bugijul@gmail.com>
2023-07-10 16:52:58 -0700

    First task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_binary - will print the equivalent value of a number
+ * @n: number to find its equivalen binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int remain, res; 
+
+	remain = n;
+	while (remain > 2)
+	{
+		res = remain
+		print
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+This is the first project that illustrates my ability on bit manipulatrion in C programming.
+It covers the tasks that are assigned to me during the ALX training program
