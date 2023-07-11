679dbf737a2ba2814040117fe5199402684e0cc3
juli868 <bugijul@gmail.com>
2023-07-11 16:56:15 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,12 +7,12 @@
 int main(int args, char **argv)
 {
 	int fl_1, fl_2, counter;
-	char copy[1024];;
+	char copy[1024];
 
 	if (args != 3)
 	{
 		printf("Usage: cp file_from file_to)\n");
-		exit (97);
+		exit(97);
 	}
 	fl_1 = open(argv[1], O_RDONLY);
 	fl_2 = open(argv[2], O_RDWR | O_CREAT);
@@ -26,9 +26,9 @@ int main(int args, char **argv)
 		printf("Error: Can't write to%s\n", argv[2]);
 		exit(99);
 	}
-	while (read(fl_1,copy,1024))
+	while (read(fl_1, copy, 1024))
 	{
-		for(counter = 0; copy[counter];counter++)
+		for (counter = 0; copy[counter]; counter++)
 		{
 			write(fl_2, &copy[counter], sizeof(copy[counter]));
 		}
