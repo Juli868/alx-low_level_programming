3211c4e14e558d0090bf90e42e515a08c6bd0f1a
juli868 <bugijul@gmail.com>
2023-07-11 15:27:10 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_RDWR | O_CREAT | O_APPEND);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC,0600);
 	if (text_content != NULL)
 		write(file, text_content, sizeof(text_content));
 	close(file);
