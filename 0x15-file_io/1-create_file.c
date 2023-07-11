9238ef28d5efe083d75ca3fb6ac8c675ad3fb3d5
juli868 <bugijul@gmail.com>
2023-07-11 14:42:47 -0700

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_content: what to put in file
+ * Return: 1 on successelse different number
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_RDWR);
+	if (text_content != NULL)
+		write(file, text_content, sizeof(text_content));
+	close(file);
+	return (1);
+}
