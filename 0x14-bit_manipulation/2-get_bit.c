eee34ad295cb720b91852da6d531af1db36ea7d3
Juli868 <bugijul@gmail.com>
2023-08-08 15:08:22 +0200

    third task

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_bit - finds if the bit at a certain index is defined r nt
+ * @n: number to check
+ * @index: index to look on
+ * Return: 1 if defined, 0 if not else -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int checker = 1;
+	unsigned long int res;
+
+	checker = checker << (index);
+	res = n & checker;
+	if (res < n)
+		return (0);
+	if (res >= n)
+		return(1);
+	return (-1);
+}
