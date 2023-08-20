5bbfe904f25f9d6b5d07f1ad3fb07d66367cff4d
Juli868 <bugijul@gmail.com>
2023-08-20 15:21:07 +0200

    LAst trial

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  */
 int main(void)
 {
-	char string[59] = {"and that piece of art is useful\" - Dora Korpar,"
-" 2015-10-19\n"};
+	char string[59] = {
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"};
 
 	write(2, string, 59);
 	return (1);
