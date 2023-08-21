3e137f6411b39d440329eb0bb0b1f6d2361208c4
Juli868 <bugijul@gmail.com>
2023-08-21 13:40:21 +0200

    First advanced task

---
+++ 0x18-dynamic_libraries/advanced.h
@@ -0,0 +1,8 @@
+#ifndef ADVANCED_H
+#define ADVANCED_H
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+float div(int a, int b);
+int mod(int a, int b);
+#endif
---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,51 @@
+#include"advanced.h"
+/**
+ * add -calculates the sum of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - finds the difference between two numbers
+ *@a: first number
+ *@b: secind number
+ *Return: the difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mod - calculates the remainder between two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+/**
+ * mul- finds the product of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: their product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - finds a quotient of two numbes
+ * @a: first number
+ * @b: second number
+ * Return: the quotient
+ */
+float div(int a, int b)
+{
+	return (a / b);
+}
