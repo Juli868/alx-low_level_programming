518880ff53cadb6d4c90cdbe2399d8ff6e846a79
Juli868 <bugijul@gmail.com>
2023-09-08 23:28:03 +0200

    First task redo

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,13 +7,23 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
-
+	int i = 0;
 	if (size <= 0)
 		return (NULL);
 	new = malloc(sizeof(hash_table_t));
+	if new == NULL
+		return (NULL)
 	new->size = size;
 	new->array = malloc(size * sizeof(hash_node_t));
-	if (new == NULL)
-		free(NULL);
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		new->array[i] = NULL;
+		i++;
+	}
 	return (new);
 }
