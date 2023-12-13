0222865d09efad8daeccd23ed2c04769e418f531
juli868 <bugijul@gmail.com>
2023-12-13 04:34:56 +0200

    First task

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * linear_search - searches in an array liearly.
+ * @array: first element
+ * @size: size of the array
+ * @value: value to look for
+ * Return: -1 if not found or the first position
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	int res;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			res = (int)i;
+			return (res);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+Dealing with huge data it is bettr to know your way around, thus the importance of this project. Since it deals with searching for some data in the specified pool.
